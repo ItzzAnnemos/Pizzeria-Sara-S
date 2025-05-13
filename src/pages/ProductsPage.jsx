6e37df03a857d89998c2972d69a7939541927e56
Nikola Serafimov <serafimovnikola582@gmail.com>
2025-05-13 02:37:12 +0200

    Spinner hotfix

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,15 +1,20 @@
 import ProductsSection from "../components/ProductsSection.jsx";
 import useMenuData from "../hooks/useMenuData.jsx";
 import ThemeToggle from "../components/ThemeToggle.jsx";
-import { Pizza } from 'lucide-react';
+import {Pizza} from 'lucide-react';
 
 function ProductsPage() {
     const {data, loading, error} = useMenuData();
 
     if (loading) return (
         <div className="flex justify-center items-center h-screen bg-amber-50 dark:bg-gray-900">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-600 dark:border-red-300"></div>
-            <Pizza className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-600 dark:text-red-400" size={24} />
+            <div className="relative">
+                <div
+                    className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-600 dark:border-red-300"></div>
+                <Pizza
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-600 dark:text-red-400"
+                    size={24}/>
+            </div>
         </div>
     );
 
@@ -26,9 +31,10 @@ function ProductsPage() {
         <div className="bg-amber-50 dark:bg-gray-900 min-h-screen flex flex-col p-4 md:p-8 w-full overflow-x-hidden">
             <header className="mb-8 text-center">
                 <div className="absolute right-1 top-3  md:right-4 md:top-4">
-                    <ThemeToggle />
+                    <ThemeToggle/>
                 </div>
-                <h1 className="text-3xl md:text-4xl font-bold text-primary-light text-red-700 dark:text-red-300 mb-2">Пицерија Сара С</h1>
+                <h1 className="text-3xl md:text-4xl font-bold text-primary-light text-red-700 dark:text-red-300 mb-2">Пицерија
+                    Сара С</h1>
                 <p className="text-gray-600 dark:text-gray-300 italic">Врвен квалитет</p>
             </header>
 
